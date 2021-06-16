c70b5e68962ef5f94e1e90977b7a9c562ad19d06
camila0606 <63520693+camila0606@users.noreply.github.com>
2021-06-16 17:53:56 -0300

    cosas

--- routes/product.js
+++ routes/product.js
@@ -20,7 +20,7 @@ var storage = multer.diskStorage({
 
 router.get('/', productController.product);
 router.get ('/add',productController.add);
-router.post('add', upload.single(bookImage), productController.store)
+//router.post('add', upload.single(bookImage), productController.store)
 router.get('/search', productController.search)
 /* GET home page. */
 //router.get('/id/:id', productController.show);
